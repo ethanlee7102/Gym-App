f7b104a937730a99425fa86b654eca7e30a57be0
ethanlee7102 <ethanlee7102@gmail.com>
2025-04-23 18:07:02 -0700

    added navigation to pages, theme, and made tab black

--- gym-app/app/_layout.tsx
+++ gym-app/app/_layout.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { ThemeProviderCustom } from '@/themes/ThemeContext';
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
